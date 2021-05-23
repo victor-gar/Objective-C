91319916610acd1040c1f2af4b1feed00129d5ad
My Name <myEmail@example.com>
2021-05-23 18:42:47 +0600

    Initial Commit

---
+++ Objective-C-basic/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  Objective-C-basic
+//
+//  Created by Victor Garitskyu on 23.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        double first = 0;
+        double second = 0;
+        printf("First number: ");
+                scanf("%lf", &first);
+        printf("Second number: ");
+                scanf("%lf", &second);
+        NSLog(@"+ Addition of numbers - %.f", first + second);
+        NSLog(@"- Substraction of numbers - %.f", first - second);
+        NSLog(@"/ Divisions of numbers - %.1f", first / second);
+        NSLog(@"* Multiplication of numbers - %.f", first * second);
+    }
+    return 0;
+}
