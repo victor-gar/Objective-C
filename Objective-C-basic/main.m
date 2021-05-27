92d39b53837e5749698805101434024d6b0389f4
Victor Garitskyu <myEmail@example.com>
2021-05-27 23:01:32 +0600

    zadani2

--- Objective-C-basic/main.m
+++ Objective-C-basic/main.m
@@ -7,20 +7,63 @@
 
 #import <Foundation/Foundation.h>
 
+int calculate(NSString *method, int a, int b) {
+   
+    if ([method isEqualToString:@"+"]) {
+        return a + b;
+    }
+    else if ([method isEqualToString:@"-"]) {
+        return a - b;
+    }
+    else if ([method isEqualToString:@"*"]) {
+        return a * b;
+    }
+    else if ([method isEqualToString:@"/"]) {
+        return a / b;
+    }
+    else if ([method isEqualToString:@"%"]) {
+        return a % b;
+    }
+    else {
+        NSLog(@"Функция не знает переданный метод");
+        return 0;
+    }
+    
+    return a + b;
+
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        double first = 0;
-        double second = 0;
-        double third = 0;
-        printf("First number: ");
-                scanf("%lf", &first);
-        printf("Second number: ");
-                scanf("%lf", &second);
-        printf("Third number: ");
-                scanf("%lf", &third);
-        NSLog(@"Addition of numbers - %.f, Substraction of numbers - %.f, Divisions of numbers - %.1f, Multiplication of numbers - %.f", first + second, first - second, first / second, first * second );
-        NSLog(@"Average of 3 numbers - %.2f", (first + second + third) / 3);
-            }
+        printf("Enter a letter to check:");
+        char cstring[1];
+        scanf("%s", cstring);
+        NSCharacterSet *validChars = [NSCharacterSet characterSetWithCharactersInString:@"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"];
+        validChars = [validChars invertedSet];
+        NSString *bukva = @(cstring);
+        NSRange  range = [bukva rangeOfCharacterFromSet:validChars];
+        if (NSNotFound != range.location) {
+        NSLog(@"Not included in the English alphabet");
+        }
+        else
+        {
+        NSLog(@"Included in the English alphabet");
+        }
+        
+        int a = calculate(@"+", 2, 3);
+               int b = calculate(@"-", 3, 2);
+               int c = calculate(@"*", 4, 5);
+               int d = calculate(@"/", 10, 2);
+               int e = calculate(@"%", 6, 3);
+               NSLog(@"Result: \n a = %i, \n b = %i, \n c = %i, \n d = %i, \n e = %i", a, b, c, d, e);
+           }
+
+    //
+        
+        
+        
+        
     return 0;
 }
+
