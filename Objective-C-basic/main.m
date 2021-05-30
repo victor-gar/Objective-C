d494a894ff1cc1069f1ef620f742c7eb12d2e81e
Victor Garitskyu <myEmail@example.com>
2021-05-30 17:28:36 +0600

    lesson2-task2verno

--- Objective-C-basic/main.m
+++ Objective-C-basic/main.m
@@ -7,31 +7,72 @@
 
 #import <Foundation/Foundation.h>
 
+int calculateadd(NSString *method, double a, double b) {
+    if ([method isEqualToString:@"+"]) {
+        return a + b;
+    }
+    else {
+        NSLog(@"Функция не знает переданный метод");
+        return 0;
+    }
+    return a + b;
+}
+int calculatesub(NSString *method, double a, double b) {
+    if ([method isEqualToString:@"-"]) {
+        return a - b;
+    }
+    else {
+        NSLog(@"Функция не знает переданный метод");
+        return 0;
+    }
+    return a - b;
+}
+int calculatemul(NSString *method, double a, double b) {
+    if ([method isEqualToString:@"*"]) {
+        return a * b;
+    }
+    else {
+        NSLog(@"Функция не знает переданный метод");
+        return 0;
+    }
+    return a * b;
+}
+double calculatediv(NSString *method, double a, double b) {
+    if ([method isEqualToString:@"/"]) {
+        return a / b;
+    }
+    else {
+        NSLog(@"Функция не знает переданный метод");
+        return 0;
+    }
+    return a / b;
+}
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        printf("Enter a letter to check:");
-        char cstring[1];
-        scanf("%s", cstring);
-        NSCharacterSet *validChars = [NSCharacterSet characterSetWithCharactersInString:@"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"];
-        validChars = [validChars invertedSet];
-        NSString *bukva = @(cstring);
-        NSRange  range = [bukva rangeOfCharacterFromSet:validChars];
-        if (NSNotFound != range.location) {
-        NSLog(@"Not included in the English alphabet");
+        double first = 0;
+        double second = 0;
+        printf("Task 2 : Calculator \n");
+        printf("Enter a first Number:");
+        scanf("%lf", &first);
+        printf("Enter a second Number:");
+        scanf("%lf", &second);
+               int a = calculateadd(@"+", first, second);
+               int b = calculatesub(@"-", first, second);
+               int c = calculatemul(@"*", first, second);
+        NSLog(@"\n Result: \n 1.addition = %i  \n 2.substraction = %i  \n 3.multiplication = %i", a, b, c);
+        if (second==0){
+            NSLog(@"\n 4.division by zero is not possible");
         }
         else
         {
-        NSLog(@"Included in the English alphabet");
-        }
+            double d = calculatediv(@"/", first, second);
+            NSLog(@"\n 4.division = %.1f", d);
+           }
         
-       
-
-    //
-        
-        
-        
-        
+           }
     return 0;
-}
+ }
+ 
 
