bcfe9e1b64d78612e218f19f21a65a9787ef2d12
Victor Garitskyu <myEmail@example.com>
2021-05-30 16:26:01 +0600

    lesson2-task2

--- Objective-C-basic/main.m
+++ Objective-C-basic/main.m
@@ -7,32 +7,6 @@
 
 #import <Foundation/Foundation.h>
 
-int calculate(NSString *method, int a, int b) {
-   
-    if ([method isEqualToString:@"+"]) {
-        return a + b;
-    }
-    else if ([method isEqualToString:@"-"]) {
-        return a - b;
-    }
-    else if ([method isEqualToString:@"*"]) {
-        return a * b;
-    }
-    else if ([method isEqualToString:@"/"]) {
-        return a / b;
-    }
-    else if ([method isEqualToString:@"%"]) {
-        return a % b;
-    }
-    else {
-        NSLog(@"Функция не знает переданный метод");
-        return 0;
-    }
-    
-    return a + b;
-
-}
-
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -51,13 +25,7 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Included in the English alphabet");
         }
         
-        int a = calculate(@"+", 2, 3);
-               int b = calculate(@"-", 3, 2);
-               int c = calculate(@"*", 4, 5);
-               int d = calculate(@"/", 10, 2);
-               int e = calculate(@"%", 6, 3);
-               NSLog(@"Result: \n a = %i, \n b = %i, \n c = %i, \n d = %i, \n e = %i", a, b, c, d, e);
-           }
+       
 
     //
         
